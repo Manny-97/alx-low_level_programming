bb8e1ff79567b955246a6ac053c5092e38875fe6
Muhammed <muhammedjimoh47@gmail.com>
2023-09-10 05:10:00 -0700

    feat: task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char a;
 	int b;
-	printf("Size of a char: %lu byte(s)\n", sizeof(char);
-	printf("Size of a int: %lu byte(s)\n", sizeof(int);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(b));
 	return (0);
 }
