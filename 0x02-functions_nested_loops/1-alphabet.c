1cb4e9b0b46e6f4e363c648cd2a8985f2acb9fb0
Muhammed <muhammedjimoh47@gmail.com>
2023-09-15 02:44:19 -0700

    feat: task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char letter[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+		_putchar(letter[i++]);
+	_putchar("\n");
+}
