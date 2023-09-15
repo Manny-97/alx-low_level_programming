9cf603ae124dcd5f8cdc97b93fbd83ddb878b490
Muhammed <muhammedjimoh47@gmail.com>
2023-09-15 03:33:29 -0700

    refactor: use i and j interchangeable

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j = 0;
+int i;
+int j = 0;
 
-	while (j < 10)
-	{
-	i = 'a';
-	while (i <= 'z')
-	{
-	_putchar(i);
-	i++;
-	}
-	_putchar('\n');
-	j++;
-	}
+while (j < 10)
+{
+i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+j++;
+}
 }
