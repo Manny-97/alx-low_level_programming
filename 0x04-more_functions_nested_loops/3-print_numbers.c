10885428a91a97cc00523b77141421b48945b732
Muhammed <muhammedjimoh47@gmail.com>
2023-09-17 18:12:50 -0700

    use char instead of int

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *print_numbers - print numbers
+ *print_numbers - print numbers 0123456789
  *
  *Return: void
  */
 void print_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
