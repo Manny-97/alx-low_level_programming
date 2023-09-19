aeaadd91b2d431bdbe3aeceec7109d7ca768ceb1
Muhammed <muhammedjimoh47@gmail.com>
2023-09-19 09:01:35 -0700

    feat: extra 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_atoi - convert a string to an integer
+ *@s: string character
+ *Return: return dest
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int min = 1;
+	unsigned int n = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			min *= -1
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			j = 1;
+			n = (n * 10) + (s[i] - '0');
+			i++;
+		}
+		if (j == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	n *= min;
+	return (n);
+}
