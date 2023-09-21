add5f2ba56d4edccc7acfcc9e1e565296c468807
Muhammed <muhammedjimoh47@gmail.com>
2023-09-20 23:35:51 -0700

    feat: task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *leer - encodes a string into 1337
+ *@c: character parameter
+ *Return: char
+ *
+ */
+char *leet(char *c)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(c + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == c[i])
+				c[i] = b[j];
+		}
+	}
+	return (s);
+}
